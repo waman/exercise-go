f61319da52b6812a2ab12436fe3e324c850558c4
waman <waman10da@gmail.com>
2017-08-23 09:56:40 +0900

    Solve some problems in chapter2, 3

--- ch2/tempconv/conv.go
+++ ch2/tempconv/conv.go
@@ -1,3 +1,10 @@
 package tempconv
 
 
+// CtoF は摂氏の温度を華氏へ変換します
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+// CtoF は摂氏の温度を華氏へ変換します
+func FToC(f Fahrenheit) Celsius { return Celsius((f-32))*5/9 }
+
+
