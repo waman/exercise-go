89386020c2bc43036ee8cd99717903f91a9b31d1
waman <waman10da@gmail.com>
2017-08-28 10:28:56 +0900

    Refactor the exercise 3.6

--- ch3/ex3_6/main.go
+++ ch3/ex3_6/main.go
@@ -16,8 +16,8 @@ func main(){
 	const(
 		xmin, ymin, xmax, ymax = -2, -2, +2, +2
 		width, height          = 1024, 1024
-		dx = float64(xmax-xmin)/width  // x 方向の次の画素までの距離
-		dy = float64(ymax-ymin)/height // y 方向の次の画素までの距離
+		dx = float64(xmax-xmin)/width  // 画素の x 方向の幅（もしくは隣り合う画素の左上端どうしの距離）
+		dy = float64(ymax-ymin)/height // 画素の y 方向の幅
 	)
 
 	img := image.NewRGBA(image.Rect(0, 0, width, height))
@@ -26,8 +26,9 @@ func main(){
 		for px := 0; px < width; px++ {
 			x := float64(px)*dx + xmin
 
+			// 1つの画素を4つのサブピクセルに分割したときの、
+			// サブピクセルの各左上端について色を計算。
 			c1 := mandelbrot(complex(x, y))
-			// 隣の画素との中間位置を「サブピクセル」の位置とする。
 			c2 := mandelbrot(complex(x+dx/2, y))
 			c3 := mandelbrot(complex(x, y+dy/2))
 			c4 := mandelbrot(complex(x+dx/2, y+dy/2))
@@ -42,9 +43,8 @@ func main(){
 	png.Encode(os.Stdout, img)
 }
 
-// 平均を計算しやすくするため、返り値を Gray にしています。
-// 200回の繰り返しの後で半径2の円の外に出ない場合の黒色も
-// Gray{0} にしています。
+// 色の平均を計算しやすくするため、返り値を Gray にしています。
+// 繰り返しを抜けた場合の黒色も Gray{0} に変更しています。
 func mandelbrot(z complex128) color.Gray {
 	const iterations = 200
 	const contrast   = 15
