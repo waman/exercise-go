44b1fa4413794495978082a552ee508d2a18e891
waman <waman10da@gmail.com>
2017-09-11 12:24:16 +0900

    Fix the exercise 4.4

--- ch4/ex4_4/main.go
+++ ch4/ex4_4/main.go
@@ -5,25 +5,63 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
+	"log"
 )
 
+// 最後の引数を除く引数を入力の文字列スライスとし、整数を与える最後の引数分だけ回転させます。
+// 実行例：
+//
+//   > go run ./ch4/ex4_4/main.go a b c d e 3
+//   > [d e a b c]
+//
 func main(){
 	// コマンドライン引数のコピーを作成
-	a := make([]string, len(os.Args)-1)
-	copy(a, os.Args[1:])
+	n := len(os.Args)
+	a := make([]string, n-2)
+	copy(a, os.Args[1:n-1])
+
+	i, err := strconv.Atoi(os.Args[n-1])
+	if err != nil  || i < 0 {
+		log.Fatalf("最後の引数は自然数にしてください： %s %s", os.Args[n-1], err)
+		return
+	}
 
 	// 回転の実行
-	rotate(a)
+	rotate(a, i)
 
 	fmt.Println(a)
 }
 
-func rotate(s []string) {
-	if n := len(s); n == 0 || n == 1 { return }
-
-	head, n := s[0], len(s)-1
-	for i := 0; i < n; i++ {
-		s[i] = s[i+1]
+func rotate(s []string, n int) {
+	N := len(s)
+	m := n%N
+	if N == 0 || N == 1 || m == 0 {
+		return
 	}
-	s[n] = head
+
+	rotateLeft(s, m)
+}
+
+func rotateLeft(s []string, n int){
+	N := len(s)
+	m := N-n
+	if N%n == 0 {
+		for i := 0; i < m; i++ {  // i+n = N の前まで
+			s[i], s[i+n] = s[i+n], s[i]
+		}
+
+	}else if 2*n < N {
+		for i := N-1; i >= m; i-- {  // i-m = 0 まで
+			s[i], s[i-m] = s[i-m], s[i]
+		}
+		rotateLeft(s[:m], n)
+
+	}else{
+		// 最後の rotateLeft() 呼び出し以外は N%n == 0 の場合と同じ
+		for i := 0; i < m; i++ {
+			s[i], s[i+n] = s[i+n], s[i]
+		}
+		rotateLeft(s[m:], N-2*m)
+	}
 }
