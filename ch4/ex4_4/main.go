6df0a8e74c2e08662587f6f40c7c4b5a1ebb68f4
waman <waman10da@gmail.com>
2017-09-13 01:05:05 +0900

    Refactor the exercise 4.4 (rotate)

--- ch4/ex4_4/main.go
+++ ch4/ex4_4/main.go
@@ -1,5 +1,5 @@
 // 【練習問題 4.4】
-// 一回のパスで操作を行う rotate を書きなさい。
+// 一回のパスで操作を行う RotateLeft を書きなさい。
 package main
 
 import (
@@ -28,40 +28,39 @@ func main(){
 	}
 
 	// 回転の実行
-	rotate(a, i)
+	RotateLeft(a, i)
 
 	fmt.Println(a)
 }
 
-func rotate(s []string, n int) {
+func RotateLeft(s []string, n int) {
 	N := len(s)
 	m := n%N
 	if N == 0 || N == 1 || m == 0 {
 		return
 	}
 
-	rotateLeft(s, m)
+	rotate(s, m)
 }
 
-func rotateLeft(s []string, n int){
+func RotateRight(s []string, n int) {
 	N := len(s)
-	m := N-n
-	if N%n == 0 {
-		for i := 0; i < m; i++ {  // i+n = N の前まで
-			s[i], s[i+n] = s[i+n], s[i]
-		}
+	m := n%N
+	if N == 0 || N == 1 || m == 0 {
+		return
+	}
 
-	}else if 2*n < N {
-		for i := N-1; i >= m; i-- {  // i-m = 0 まで
-			s[i], s[i-m] = s[i-m], s[i]
-		}
-		rotateLeft(s[:m], n)
+	rotate(s, N-m)
+}
 
-	}else{
-		// 最後の rotateLeft() 呼び出し以外は N%n == 0 の場合と同じ
-		for i := 0; i < m; i++ {
-			s[i], s[i+n] = s[i+n], s[i]
-		}
-		rotateLeft(s[m:], N-2*m)
+func rotate(s []string, n int){
+	N := len(s)
+	i := 0
+	for m := N-n; i < m; i++ {  // i+n = N-1 まで
+		s[i], s[i+n] = s[i+n], s[i]
+	}
+
+	if m := N%n; m != 0 {
+    rotate(s[i:], n-m)
 	}
 }
