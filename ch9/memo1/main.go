32b4b17823be113b6133b6c4d681beb7ea15bd71
waman <waman10da@gmail.com>
2017-10-11 08:50:52 +0900

    Add some sample codes in Chapter 9

--- ch9/memo1/main.go
+++ ch9/memo1/main.go
@@ -1,7 +1,32 @@
-// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
-// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
-//
-// memo1 パッケージは Func 型の関数の並行的に安全でないメモ化を提供します。
-package memo1
+package main
 
+import (
+	"github.com/waman/exercise-go/ch9/memo1/memo"
+	"net/http"
+	"io/ioutil"
+	"time"
+	"log"
+	"fmt"
+	"os"
+)
 
+func main(){
+	m := memo.New(httpGetBody)
+	for _, url := range os.Args[1:] {  // 本文から少し修正
+		start := time.Now()
+		value, err := m.Get(url)
+		if err != nil {
+			log.Print(err)
+		}
+		fmt.Printf("%s, %s, %d bytes\n", url, time.Since(start), len(value.([]byte)))
+	}
+}
+
+func httpGetBody(url string) (interface{}, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+	return ioutil.ReadAll(resp.Body)
+}
---
+++ ch9/memo1/memo/main.go
@@ -0,0 +1,31 @@
+// Copyright © 2016 Alan A. A. Donovan & Brian W. Kernighan.
+// License: https://creativecommons.org/licenses/by-nc-sa/4.0/
+//
+// memo1 パッケージは Func 型の関数の並行的に安全でないメモ化を提供します。
+package memo
+
+type Memo struct {
+	f     Func
+	cache map[string]result
+}
+
+type Func func(key string) (interface{}, error)
+
+type result struct {
+	value interface{}
+	err   error
+}
+
+func New(f Func) *Memo {
+	return &Memo{f: f, cache: make(map[string]result)}
+}
+
+// 注意：並行的に安全ではない！
+func (memo *Memo) Get(key string) (interface{}, error) {
+	res, ok := memo.cache[key]
+	if !ok {
+		res.value, res.err = memo.f(key)
+		memo.cache[key] = res
+	}
+	return res.value, res.err
+}
